e11cf0e819be5bf1fd50adafd728c452034daf58
Sakuraii <pearl.k.sakura@gmail.com>
2023-02-07 14:00:09 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HexGridDemo from "./component/hexagongrid/Grid";
+
+function App() {
+  return (
+    <div className="App">
+     <HexGridDemo/>
+    </div>
+  );
+}
+
+export default App;
